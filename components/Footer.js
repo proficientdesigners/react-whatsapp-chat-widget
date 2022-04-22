7877ddb5037c8043cc9109119b19b9847057e256
Proficient Designers <41690645+proficientdesigners@users.noreply.github.com>
2022-04-22 13:17:20 +0530

    bug fix
    After the chat box closes the chat box was not hidden properly now it's been  rectified

--- components/Footer.js
+++ components/Footer.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 export default function Footer(props) {
   const footerBgColor = props.footerBgColor ? props.footerBgColor : 'rgb(245, 245, 245)',
-        btnTxtColor = props.btnTxtColor ? props.btnTxtColor : 'rgb(255, 255, 255)',
         btnTxt = props.btnTxt ? props.btnTxt : 'Start Chat',
+        btnTxtColor = props.btnTxtColor ? props.btnTxtColor : 'rgb(255, 255, 255)',
         btnBgColor = props.btnBgColor ? props.btnBgColor : 'rgb(79, 206, 93)',
         phoneNo = props.phoneNo ? props.phoneNo : false;
   const [message, setMessage] = useState(props.messageBoxTxt ? encodeURI(props.messageBoxTxt) : '');
